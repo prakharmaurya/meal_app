85f03bdfd6f625cc6ceebbbd8d6775c9515b663f
Excel <000.excel@gmail.com>
2020-05-30 19:02:41 +0530

    Refactor: NamedRroute (pushNamed ) implemented insted of push

--- lib/category_item.dart
+++ lib/category_item.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:recipesapp/category_meals_screen.dart';
 
 class CategoryItem extends StatelessWidget {
   final String id;
@@ -9,12 +8,9 @@ class CategoryItem extends StatelessWidget {
   CategoryItem(this.id, this.title, this.color);
 
   void selectCategory(BuildContext context) {
-    Navigator.of(context).push(
-      MaterialPageRoute(
-        builder: (_) {
-          return CategoryMealsScreen(id, title);
-        },
-      ),
+    Navigator.of(context).pushNamed(
+      '/category-meals',
+      arguments: {'id': this.id, 'title': this.title},
     );
   }
 
