0c02bcff65973faf6330884daa245a7b2b216812
Excel <000.excel@gmail.com>
2020-05-31 08:39:11 +0530

    Add: Named Route via const applied

--- lib/main.dart
+++ lib/main.dart
@@ -35,7 +35,7 @@ class MyApp extends StatelessWidget {
       initialRoute: '/', //default route is '/'
       routes: {
         '/': (context) => CategoriesScreen(),
-        '/category-meals': (context) => CategoryMealsScreen()
+        CategoryMealsScreen.routeName: (context) => CategoryMealsScreen()
       },
     );
   }
